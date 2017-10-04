225968862d932a071bc0d30d187914f0889ce465
kang <694206216@qq.com>
2017-10-04 13:53:29 +0800

    update_testString

--- src/designpatrern/factory/factorymethod/FactoryMethodTest.java
+++ src/designpatrern/factory/factorymethod/FactoryMethodTest.java
@@ -5,11 +5,13 @@ public class FactoryMethodTest {
 	public static void main(String[] args) {
 
 		//创建iphone过程，先建工厂，再在工厂中创建iphone
-		IphoneFactory facIphone = new IphoneFactory();
-		Iphone iphone = facIphone.createPhone();
-		//创建miphone过程：
-		MiphoneFactory facMiphone = new MiphoneFactory();
-		Miphone miphone = facMiphone.createPhone();
+//		IphoneFactory facIphone = new IphoneFactory();
+//		Iphone iphone = facIphone.createPhone();
+//		//创建miphone过程：
+//		MiphoneFactory facMiphone = new MiphoneFactory();
+//		Miphone miphone = facMiphone.createPhone();
+		System.out.println('\ua321');
+		System.out.println("adgdfa".getBytes()[1]);
 	}
 
 }
