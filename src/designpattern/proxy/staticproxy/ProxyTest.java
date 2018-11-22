25b0111dccc4497e4d09581f329b5f4b8ab5ba77
kangshan <694206216@qq.com>
2018-11-22 17:42:50 +0800

    ''

--- src/designpattern/proxy/staticproxy/ProxyTest.java
+++ src/designpattern/proxy/staticproxy/ProxyTest.java
@@ -1,5 +1,10 @@
 package designpattern.proxy.staticproxy;
 
+/**
+ * @description
+ * @author      KangShan
+ * @date        2018/11/22 17:41
+ */
 public class ProxyTest {
 
 	public static void main(String[] args) {
