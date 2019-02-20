e6ca035740b0467c3866ba95653fd7064c637d54
kangshan <694206216@qq.com>
2019-02-20 18:31:02 +0800

    'add-quick-sort-demo'

--- src/designpattern/singleton/Test.java
+++ src/designpattern/singleton/Test.java
@@ -9,5 +9,29 @@ public class Test {
 
     public static void main(String[] args){
         System.out.println(StaticInnerClassSingleton.getInstance());
+
+    }
+
+
+    int[] arrayQuickSort(int[] numbers){
+
+        if(numbers.length == 1){
+            return numbers;
+        }else if(numbers.length == 0){
+            throw new IllegalArgumentException("参数错误");
+        }else if(numbers.length == 2){
+            if(numbers[0] <= numbers[1]){
+                return numbers;
+            }else {
+                return new int[]{numbers[1], numbers[0]};
+            }
+        }else {
+            int pivotIndex = numbers.length,
+                    leftIndex = 0,
+                    rightIndex= numbers.length - 1;
+            
+
+            return arrayQuickSort(numbers);
+        }
     }
 }
