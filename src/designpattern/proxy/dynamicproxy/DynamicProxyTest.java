507f1356c6676de5bb14ea8f36074cf7de72fa89
kangshan <694206216@qq.com>
2019-05-14 10:04:07 +0800

    'update'

--- src/designpattern/proxy/dynamicproxy/DynamicProxyTest.java
+++ src/designpattern/proxy/dynamicproxy/DynamicProxyTest.java
@@ -13,6 +13,9 @@ public class DynamicProxyTest {
 	 */
 	public static void main(String[] args) {
 
+	    Integer a = 1___1;
+		assert a == 1 : "the a != 1";
+
 		//动态代理的实现
 		Draw realDraw = new RealDraw();
 //		Draw proxyDraw = (Draw)new ProxyDraw(realDraw).getProxyIns2();
---
+++ src/designpattern/proxy/dynamicproxy/TestEnum.java
@@ -0,0 +1,12 @@
+package designpattern.proxy.dynamicproxy;
+
+public enum TestEnum {
+
+    SUMMER,
+    SPRING;
+
+    TestEnum(){
+
+    }
+
+}
