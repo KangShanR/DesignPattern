e6783c7953103421d10935c555e52cec46bba302
kangshan <694206216@qq.com>
2019-05-14 20:23:16 +0800

    'update-string.format'

--- src/designpattern/proxy/dynamicproxy/DynamicProxyTest.java
+++ src/designpattern/proxy/dynamicproxy/DynamicProxyTest.java
@@ -3,6 +3,8 @@
  */
 package designpattern.proxy.dynamicproxy;
 
+import java.util.Date;
+
 /**
  * @author Administrator
  */
@@ -13,8 +15,11 @@ public class DynamicProxyTest {
 	 */
 	public static void main(String[] args) {
 
+		System.out.println(String.format("%03d-%tT%n",1,new Date()));
+		System.out.println(String.format("%b-%012d",true,232));
+
 	    Integer a = 1___1;
-		assert a == 1 : "the a != 1";
+//		assert a == 1 : "the a != 1";
 
 		//动态代理的实现
 		Draw realDraw = new RealDraw();
