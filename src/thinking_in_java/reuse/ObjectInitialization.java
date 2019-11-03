0b8740724192588f671f056ed3b5ca84a41919c0
kang <694206216@qq.com>
2019-11-04 00:14:48 +0800

    Thinking in java - initialization in instance and class

---
+++ src/thinking_in_java/reuse/ObjectInitialization.java
@@ -0,0 +1,27 @@
+package thinking_in_java.reuse;
+
+/**
+ * @author KangShan
+ * @date 2019/11/3 23:33
+ */
+public class ObjectInitialization extends BaseObjectInitialization{
+//    static {
+//        System.out.println("static block ...");
+//    }
+//    static int j = assign(33);
+
+    int i = assign(11);
+
+    ObjectInitialization(){
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("construct ...");
+        ObjectInitialization objectInitialization = new ObjectInitialization();
+//        new BaseObjectInitialization();
+        System.out.println("construct end");
+
+    }
+}
