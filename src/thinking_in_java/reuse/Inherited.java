d16befda556c75828f6c8899652b2fcd6e880cb4
kang <694206216@qq.com>
2019-11-03 22:43:00 +0800

    Thinking in java - inherit points

--- src/thinking_in_java/reuse/Inherited.java
+++ src/thinking_in_java/reuse/Inherited.java
@@ -1,25 +1,47 @@
 package thinking_in_java.reuse;
 
+import lombok.Getter;
+
 /**
  * @author KangShan
  * @date 2019/10/31 23:59
  */
 public class Inherited {
 
+    @Getter
+    private Integer count;
+
+    /**
+     * executed before constructor
+     */
+    {
+        System.out.println("new Inherited instance saying ...");
+    }
+
+
+    protected Inherited(){
+        System.out.println("Inherited construct...");
+    }
+
+    protected Inherited(int count){
+        this.count = count;
+        System.out.println("Inherited construct with count: " + count);
+    }
+
     /**
      * 此番定义只将数组声明与数组长度声明，并没有将元素构造出来
      */
-    private InheritConstructor[] constructors = new InheritConstructor[4];
+//    private InheritConstructor[] constructors = new InheritConstructor[4];
 
     /**
      * 显示如此定义变量，在构造对象时就将成员构造出来
      */
-    private InheritConstructor constructor = new InheritConstructor();
+//    private InheritConstructor constructor = new InheritConstructor();
 
 
     public static void main(String[] args){
-        Inherited inherited = new Inherited();
-        System.out.println(inherited.constructors[0]);
-        System.out.println(inherited.constructor);
+        Inherited inherited = new Inherited(3);
+//        System.out.println(inherited.constructors[0]);
+//        System.out.println(inherited.constructor);
     }
 }
