ddded037121e71b93bcb0e960d19cccc69a28715
kang <694206216@qq.com>
2019-11-04 22:43:53 +0800

    test codes for initialization

--- src/thinking_in_java/reuse/AddressVsValueTransfer.java
+++ src/thinking_in_java/reuse/AddressVsValueTransfer.java
@@ -7,13 +7,30 @@ package thinking_in_java.reuse;
  */
 public class AddressVsValueTransfer {
 
+    int i;
+    AddressVsValueTransfer a;
+
+    {
+        System.out.println("auto construct");
+        /**
+         * 递归
+         */
+        //        a = new AddressVsValueTransfer();
+    }
 
     static void assign(int i){
+
         i++;
         System.out.println(i);
     }
 
     public static void main(String [] args){
+        AddressVsValueTransfer addressVsValueTransfer = new AddressVsValueTransfer() {
+            {
+                i = 222;
+            }
+        };
+        System.out.println(addressVsValueTransfer.i);
         int i = 1;
         assign(i);
         System.out.println(i);
