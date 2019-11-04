3df6bf955b2a71ed78d196363e912487020a54ef
kangshan <694206216@qq.com>
2019-11-04 20:22:23 +0800

    add hot class load test

---
+++ src/thinking_in_java/class_load/ClassLoadTest.java
@@ -0,0 +1,12 @@
+package thinking_in_java.class_load;
+
+/**
+ * @author KangShan
+ * @date 2019/11/4 19:19
+ */
+public class ClassLoadTest {
+    void print(){
+        System.out.println("doogy");
+    }
+
+}
---
+++ src/thinking_in_java/class_load/MyClassLoader.java
@@ -0,0 +1,35 @@
+package thinking_in_java.class_load;
+
+import java.io.*;
+
+/**
+ * @author KangShan
+ * @date 2019/11/4 19:19
+ */
+public class MyClassLoader extends ClassLoader {
+    String classPath;
+
+    MyClassLoader(String classPath){
+        super(ClassLoader.getSystemClassLoader());
+        this.classPath = classPath;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) {
+        name = name.replace(".", "//");
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (FileInputStream fileInputStream = new FileInputStream(new File(classPath + name + ".class"))) {
+            int i = 0;
+            while((i = fileInputStream.read()) != -1){
+                byteArrayOutputStream.write(i);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return super.defineClass(name, bytes, 0, bytes.length);
+    }
+}
