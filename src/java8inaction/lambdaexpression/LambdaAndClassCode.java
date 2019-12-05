a9ed4791b8d319ab8fcae6464022f91818494370
kang <694206216@qq.com>
2019-12-06 01:05:27 +0800

    update codes

--- src/java8inaction/lambdaexpression/LambdaAndClassCode.java
+++ src/java8inaction/lambdaexpression/LambdaAndClassCode.java
@@ -1,15 +1,22 @@
 package java8inaction.lambdaexpression;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 /**
+ * late-binding
+ * dynamic invoke
  * @author KangShan
  * @date 2019/12/4 0:21
  */
 public class LambdaAndClassCode {
     Function function = p -> p;
 
-//    public static void main(String[] args) {
-//
-//    }
+    public static void main(String[] args) {
+        List<BigDecimal> bigDecimals = Arrays.asList(BigDecimal.ZERO, BigDecimal.ONE, BigDecimal.TEN);
+        bigDecimals.set(0, BigDecimal.valueOf(32));
+        bigDecimals.stream().map(b -> b.add(BigDecimal.ONE)).forEach(System.out::println);
+    }
 }
