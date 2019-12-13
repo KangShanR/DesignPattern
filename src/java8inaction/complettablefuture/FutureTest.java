911d88ec10af9d082692920c20afb857e01f465d
kang <694206216@qq.com>
2019-12-14 00:56:43 +0800

    update codes of Future & CompletableFuture

--- src/java8inaction/complettablefuture/FutureTest.java
+++ src/java8inaction/complettablefuture/FutureTest.java
@@ -8,17 +8,12 @@ import java.util.concurrent.*;
  */
 public class FutureTest {
 
-    public static void main(String[] args) {
-//        ThreadPoolExecutor
-//        ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(1,
-//                r -> {
-//                    System.out.println("new Thread Factory");
-//                    return new Thread(r);
-//                });
+    static void executorServiceTest() {
         ExecutorService pool = Executors.newCachedThreadPool();
         Future<Integer> future         = pool.submit(() -> {
             try {
                 Thread.sleep(2900);
+                throw new RuntimeException("exception");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -26,15 +21,60 @@ public class FutureTest {
         });
         System.out.println("wait");
         try {
-            System.out.println(future.get(3, TimeUnit.SECONDS));
+            System.out.println(future.get());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
         }
 
-        pool.shutdown();
+//        pool.shutdown();
     }
+
+    static void threadPoolExecutorTest() {
+        ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(1,
+                r -> {
+                    System.out.println("new Thread Factory");
+                    return new Thread(r);
+                });
+    }
+
+    static void completableFutureTest() {
+        CompletableFuture<String> good = CompletableFuture.supplyAsync(() -> {
+            throw new RuntimeException("good");
+        });
+        try {
+            String o = good.get();
+            System.out.println(o);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Future<String> getF() {
+        CompletableFuture<String> future = new CompletableFuture<>();
+        new Thread(()-> {
+            String str = null;
+            future.complete((String) str.concat("d"));
+        }).start();
+        return future;
+    }
+
+    public static void main(String[] args) {
+//        ExecutorServiceTest();
+//        completableFutureTest();
+        executorServiceTest();
+//        try {
+//            System.out.println(getF().get(5, TimeUnit.SECONDS));
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (TimeoutException e) {
+//            e.printStackTrace();
+//        }
+    }
+
 }
