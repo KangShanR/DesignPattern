bfd03cf0ac8f654bc85a56988730a5e1fb693a10
kangshan <694206216@qq.com>
2019-12-19 19:15:21 +0800

    add notes

--- src/java8inaction/completablefuture/ScheduledThreadPoolExecutorTest.java
+++ src/java8inaction/completablefuture/ScheduledThreadPoolExecutorTest.java
@@ -9,11 +9,15 @@ import java.util.concurrent.TimeUnit;
  * @date 2019/12/19 18:45
  */
 public class ScheduledThreadPoolExecutorTest {
+    /**
+     * 测试固定延迟 schedule task, 与 fixedRate 不同在于其固定间隔时间只在执行完成后再开始计时 delay
+     * @date    2019/12/19 19:10
+     */
     static void fixedDelayTest() {
         ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(10,
                 r -> {
                     Thread thread = new Thread(r);
-                    System.out.println("new Thread : " + thread.getName() );
+                    System.out.println("new Thread : " + thread.getName());
                     return thread;
                 });
 
@@ -30,11 +34,17 @@ public class ScheduledThreadPoolExecutorTest {
                 5, 2, TimeUnit.SECONDS);
     }
 
+    /**
+     * 测试固定执行 scheduled task
+     * 同时，只有当前一个任务执行完成后才会执行第二个任务，不管有没有达到后一次 task 的 fixed 时间。
+     * 也就是在这种情况下， fixed rate 会失效
+     * @date    2019/12/19 19:09
+     */
     static void fixedRateTest() {
         ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(10,
                 r -> {
                     Thread thread = new Thread(r);
-                    System.out.println("new Thread : " + thread.getName() );
+                    System.out.println("new Thread : " + thread.getName());
                     return thread;
                 });
 
@@ -43,7 +53,7 @@ public class ScheduledThreadPoolExecutorTest {
                             "scheduled task time:" + LocalDateTime.now() +
                                     "; Thread:" + Thread.currentThread().getName());
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(3000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
