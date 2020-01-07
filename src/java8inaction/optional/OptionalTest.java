0e8d651e093b8df38df0e84609f7ed52d6f97d8e
kangshan <694206216@qq.com>
2020-01-07 13:56:01 +0800

    test codes of inner class

--- src/java8inaction/optional/OptionalTest.java
+++ src/java8inaction/optional/OptionalTest.java
@@ -9,7 +9,10 @@ import java.util.function.Function;
  * @date 2019/12/10 23:54
  */
 public class OptionalTest {
-
+    /**
+     * 用以查看编译器生成 dynamic invoke 指令。
+     * lambda expression 全都通过动态调用
+     */
     Function<Integer, String> f = (Integer i) -> i.toString();
     Consumer<Integer> c = (Integer i) -> {};
 
---
+++ src/thinking_in_java/innerclass/LocalInnerClass.java
@@ -0,0 +1,48 @@
+package thinking_in_java.innerclass;
+
+/**
+ * 局部内部类
+ * @author KangShan
+ * @date 2020/1/6 13:48
+ */
+public class LocalInnerClass {
+    void method() {
+        System.out.println("method print");
+    }
+    public static void main(String[] args) {
+        /* Local inner class */
+        class LocalInnerClass2 {
+            void method2() {
+                System.out.println("method2 print");
+            }
+        }
+        //local class instance
+        new LocalInnerClass2().method2();
+        //anonymous inner class instance
+        LocalInnerClass localInnerClass = new LocalInnerClass() {
+            /**
+             * compile error
+             * you can not initialize a instance in anonymous inner class
+             */
+//            LocalInnerClass(String string) {
+//                System.out.println(string);
+//            }
+            //override the method
+            @Override
+            void method() {
+                System.out.println("override method");
+                super.method();
+            }
+
+            void method2(){
+                System.out.println("method2 initialize");
+            }
+            //after initialize
+            {
+
+                System.out.println("post initialize");
+                method2();
+            }
+        };
+    }
+}
