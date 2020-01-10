a72109d17306cf10504897b5e4273c7e28918b0d
kang <694206216@qq.com>
2020-01-10 21:55:26 +0800

    add notes of anonymous inner class

--- src/thinking_in_java/innerclass/InnerClassTest.java
+++ src/thinking_in_java/innerclass/InnerClassTest.java
@@ -20,6 +20,8 @@ public class InnerClassTest extends Outer.Inner2 {
          * 匿名内部类的创建使用，第一个 `{}` 内重写类结构，可重写方法与初始化语句；
          *      第二个 `{}` 在第一个 `{}` 之中（无所谓与其他定义语句的先后顺序），
          *      写下这个实例的执行语句，可调用这个实例的方法。
+         * @ATTENTION Local Inner Class 局部类有更好的特性：有名-> 可以重写构造函数
+         *      也就可以构造多个重写后的类的对象
          */
         new Outer() {
             {
