89a5b629cb4dce5583fd2353718238f912a4ef30
kangshan <694206216@qq.com>
2020-06-04 18:39:32 +0800

    obscure binary search

--- src/algorithm/BinarySearch.java
+++ src/algorithm/BinarySearch.java
@@ -7,12 +7,36 @@ package algorithm;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] array = {1, 2, 3,3,3, 4,4,4,4, 5,5,5,5, 6, 7, 8,8,8,8};
-        System.out.println(search(array, 8, 0, array.length - 1, false));
+        int[] array1 = {1,1};
+        System.out.println(search(array1, 0, 0, array1.length - 1, true));
     }
 
     static int search(int[] array, int value, int lo, int hi, boolean firstOne) {
-        if (lo > hi) {
-            return -1;
+        if (hi == lo) {
+            return array[lo] == value ? lo : -1;
+        }
+        int i = lo + (hi - lo >> 1);
+        int target = array[i];
+        if (target == value) {
+            if(firstOne) {
+                while (i > 1 && array[--i] == value) {}
+                return i > 1 ? ++i:i;
+            }else {
+                while (i< array.length - 1 && array[++i] == value) {}
+                return i < array.length - 1 ? --i:i;
+            }
+        } else if (target > value) {
+            return i == 0 ? -1 : search(array, value, lo, i - 1, firstOne);
+        } else {
+            return (i == array.length - 1) ? -1 : search(array, value, i + 1, hi, firstOne);
+        }
+    }
+
+    static int obscureSearch(int[] array, int value, int lo, int hi, boolean firstOne) {
+        if (array[lo] == value) {
+            return lo;
+        } else if (array[hi] == value) {
+            return hi;
         }
         int i = lo + (hi - lo >> 1);
         int target = array[i];
@@ -24,6 +48,8 @@ public class BinarySearch {
                 while (i< array.length && array[i] == value) {i++;}
                 return i;
             }
+        } else if (i - lo == 1) {
+            return firstOne ? lo : i;
         } else if (target > value) {
             return search(array, value, lo, i - 1, firstOne);
         } else {
