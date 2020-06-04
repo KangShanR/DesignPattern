d16d2b853400e760f68ccb8c0fb88a35ba975059
kangshan <694206216@qq.com>
2020-06-04 11:30:35 +0800

    binary search in sorted array has same elements

--- src/algorithm/BinarySearch.java
+++ src/algorithm/BinarySearch.java
@@ -6,22 +6,28 @@ package algorithm;
  */
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        System.out.println(search(array, 3, 0, array.length - 1));
+        int[] array = {1, 2, 3,3,3, 4,4,4,4, 5,5,5,5, 6, 7, 8,8,8,8};
+        System.out.println(search(array, 8, 0, array.length - 1, false));
     }
 
-    static int search(int[] array, int value, int lo, int hi) {
+    static int search(int[] array, int value, int lo, int hi, boolean firstOne) {
         if (lo > hi) {
             return -1;
         }
         int i = lo + (hi - lo >> 1);
         int target = array[i];
         if (target == value) {
-            return i;
+            if(firstOne) {
+                while (i > -1 && array[i] == value) {i--;}
+                return i;
+            }else {
+                while (i< array.length && array[i] == value) {i++;}
+                return i;
+            }
         } else if (target > value) {
-            return search(array, value, lo, i - 1);
+            return search(array, value, lo, i - 1, firstOne);
         } else {
-            return search(array, value, i + 1, hi);
+            return search(array, value, i + 1, hi, firstOne);
         }
     }
 }
